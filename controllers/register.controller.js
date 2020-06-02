3a02b4dc9334b57f12c89e612293b46f19b7c1dd
imdhruv99 <dpdhruvprajapati@gmail.com>
2020-06-02 18:08:14 +0530

    user login-registration and crud using MEAN Stack

---
+++ controllers/register.controller.js
@@ -0,0 +1,36 @@
+﻿var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('config.json');
+
+router.get('/', function (req, res) {
+    res.render('register');
+});
+
+router.post('/', function (req, res) {
+    // register using api to maintain clean separation between layers
+    request.post({
+        url: config.apiUrl + '/users/register',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {
+        if (error) {
+            return res.render('register', { error: 'An error occurred' });
+        }
+
+        if (response.statusCode !== 200) {
+            return res.render('register', {
+                error: response.body,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                username: req.body.username
+            });
+        }
+
+        // return to login page with success message
+        req.session.success = 'Registration successful';
+        return res.redirect('/login');
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+
+// routes
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
+
+// start server
+var server = app.listen(7500, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
